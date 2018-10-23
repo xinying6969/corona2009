ff4bbe01ca431156d5a9ef26638349f1023901a6
mkwhite <ying.6969@github.com>
2018-10-23 09:24:12 +0800

    asd
    hei u still  suck

---
+++ main.lua
@@ -0,0 +1,16 @@
+
+-- Abstract: Hello World
+-- Version: 2.0
+-- Sample code is MIT licensed; see https://www.coronalabs.com/links/code/license
+---------------------------------------------------------------------------------------
+
+local world1 = display.newImageRect( "world.png", 150, 150 )
+world1.x = display.contentCenterX
+world1.y = display.contentCenterY + 150
+
+local world2 = display.newImageRect( "world.png", 150, 150 )
+world2.x = display.contentCenterX
+world2.y = display.contentCenterY - 150
+
+local msgText = display.newText( "D1044172058", world1.x, world1.y-150, native.systemFont, 32 )
+msgText:setFillColor( 1, 1, 1 )

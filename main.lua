7eda268cb1efd9fb1f85a01d5e1a1312f1e6f8ab
icecreamqii <44384698+icecreamqii@users.noreply.github.com>
2018-10-23 09:55:43 +0800

    The Second hello world added
    rt

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 local msgText = display.newText( "Hello World!", 150, 200, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
 
+local msgText = display.newText( "Hello World!", 150, 400, native.systemFont, 32 )
+msgText:setFillColor( 0.2, 0.6, 0.8 )
 

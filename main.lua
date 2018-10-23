b1be593790513fbaf3a633362650baeb3b7f21a3
icecreamqii <44384698+icecreamqii@users.noreply.github.com>
2018-10-23 09:50:03 +0800

    change to hello world
    student NO change to hello world alrdy

--- main.lua
+++ main.lua
@@ -12,7 +12,5 @@ local world2 = display.newImageRect( "world.png", 150, 150 )
 world2.x = display.contentCenterX
 world2.y = display.contentCenterY - 150
 
-local msgText = display.newText( "D1064423059", world1.x, world1.y-150, native.systemFont, 32 )
-msgText:setFillColor( 1, 1, 1 )
-
-print ("hello world")
+local msgText = display.newText( "Hello World!", world1.x, world1.y-150, native.systemFont, 32 )
+msgText:setFillColor( 0.2, 0.6, 0.8 )
